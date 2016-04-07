2c5ce435a01594230fba6693d946c353216d1d43
Brandon Ly <lypanda1@gmail.com>
2016-04-06 19:23:43 -0700

    initial commit

---
+++ www/js/directives/menu.js
@@ -0,0 +1,11 @@
+angular.module('CCSD_Board')
+
+.directive('menu', function() {
+    return {
+        templateUrl: '../templates/menu.html',
+        controller: 'mainCtrl'
+    };
+
+});
+
+console.log('menu.js was loaded');
